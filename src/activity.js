5e208b730b0fcc76ba1a8d2c5dfd0bd356fc74bc
Cameron MacRae <47998896+cammac60@users.noreply.github.com>
2019-10-16 10:13:52 -0600

    Revert "Andrew13"

--- src/activity.js
+++ src/activity.js
@@ -24,10 +24,14 @@ class Activity {
       return matchedElem[key];
     }
 
-  returnUserMiles(activityData, curDate) {
-    let matchedElem = activityData.find(elem => {
-      if (elem.userID === this.userID && elem.date === curDate) {
-        return elem
+    returnWeekAvg(data, date) {
+      let week = [];
+      let user = data.filter(elem => {
+        return elem.userID === this.userID;
+      });
+      let index = user.findIndex(elem => elem.date === date)
+      for(let i = index; i < (index + 7); i++) {
+        week.push(user[i]);
       }
       let total = week.reduce((acc, curVal) => {
         acc += curVal['minutesActive'];
@@ -45,61 +49,32 @@ class Activity {
       } else {
         return false;
       }
-    })
-    return matchedElem.minutesActive;
-  }
-
-  returnWeekAvg(data, date) {
-    let week = [];
-    let user = data.filter(elem => {
-      return elem.userID === this.userID;
-    });
-    let index = user.findIndex(elem => elem.date === date)
-    for (let i = index; i < (index + 7); i++) {
-      week.push(user[i]);
     }
-    let total = week.reduce((acc, curVal) => {
-      acc += curVal['minutesActive'];
-      return acc;
-    }, 0)
-    return (total / 7);
-  }
 
-  matchGoal(date, data) {
-    let user = data.find(elem => {
-      return elem.userID === this.userID && elem.date === date;
-    })
-    if (user.numSteps >= this.stepGoal) {
-      return true;
-    } else {
-      return false;
+    checkBestDays(data) {
+      let bestDays = [];
+      let week = data.filter(elem => {
+        return elem.userID === this.userID && elem.numSteps >= this.stepGoal;
+      })
+      week.map(elem => {
+        bestDays.push(elem.date);
+      })
+      return bestDays;
     }
-  }
 
-  checkBestDays(data) {
-    let bestDays = [];
-    let week = data.filter(elem => {
-      return elem.userID === this.userID && elem.numSteps >= this.stepGoal;
-    })
-    week.map(elem => {
-      bestDays.push(elem.date);
-    })
-    return bestDays;
-  }
+    checkRecord(data) {
+      let user = data.filter(elem => {
+        return elem.userID === this.userID
+      })
+      let recordDay = user.sort((a, b) => b.flightsOfStairs - a.flightsOfStairs);
+      return recordDay[0].date;
+    }
 
-  checkRecord(data) {
-    let user = data.filter(elem => {
-      return elem.userID === this.userID
-    })
-    let recordDay = user.sort((a, b) => b.flightsOfStairs - a.flightsOfStairs);
-    return recordDay[0].date;
-  }
-
-  checkUserAvgs(date, key, data) {
-    let keyValues = [];
-    let numsToAvg = data.filter(elem => {
-      return elem.date === date;
-    })
+    checkUserAvgs(date, key, data) {
+      let keyValues = [];
+      let numsToAvg = data.filter(elem => {
+        return elem.date === date;
+      })
       .map(elem => keyValues.push(elem[key]))
       let total = keyValues.reduce((acc, cur) => acc += cur)
       return (total / keyValues.length)
